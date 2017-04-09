8065888ace07110bf547cb5aa9b0dfba445f4d1f
phatnvm <phat4everduke@gmail.com>
2017-04-09 19:18:30 +0700

    Connect to xampp
    database: mydatabase
    table: qlnv2

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace DataConnection2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btOpen = new System.Windows.Forms.Button();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(139, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(509, 343);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btOpen
+            // 
+            this.btOpen.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btOpen.Location = new System.Drawing.Point(12, 60);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(121, 32);
+            this.btOpen.TabIndex = 2;
+            this.btOpen.Text = "Open";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(12, 34);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.ShortcutsEnabled = false;
+            this.tbIP.Size = new System.Drawing.Size(202, 20);
+            this.tbIP.TabIndex = 3;
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl + S";
+            this.addToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.addToolStripMenuItem.ShowShortcutKeys = false;
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.addToolStripMenuItem.Text = "Save";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.Delete;
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(660, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 413);
+            this.Controls.Add(this.tbIP);
+            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,329 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataConnection2
+{
+    public partial class Form1 : Form
+    {
+        //SqlCommand mComm;
+        // SqlConnection mConn;
+        DataTable mTable;
+        //SqlDataAdapter mAdapter;
+        SqlCommandBuilder cb;
+
+        MySqlConnection mConn;
+        MySqlCommand mComm;
+        MySqlDataAdapter mAdapter;
+
+        String tableName = "qlnv2";
+        //String connectionString = "Server=.;Database=MyDatabase;Trusted_Connection=true";
+        String connectionString = "";
+        //String str;
+        String tableCommand;
+        String tableSelection = "select * from qlnv2";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // dataGridView1.Dock = DockStyle.Fill;
+            this.ActiveControl = btOpen;
+            //mConn = new SqlConnection("Server=.;Integrated Security=SSPI;Database=master");
+            cb = new SqlCommandBuilder();
+
+            //    str = "CREATE DATABASE mydatabase ON PRIMARY " +
+            //"(NAME = DataConnection2, " +
+            //"FILENAME = 'D:\\DataConnectionData.mdf', " +
+            //"SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%) " +
+            //"LOG ON (NAME = DataConnection2_Log, " +
+            //"FILENAME = 'D:\\DataConnectionLog.ldf', " +
+            //"SIZE = 1MB, " +
+            //"MAXSIZE = 5MB, " +
+            //"FILEGROWTH = 10%)";
+
+            tableCommand = "CREATE TABLE IF NOT EXISTS " + tableName + " (" +
+            "ID char(10) primary key, " +
+            "Name char(50));";
+
+            //SqlConnectionStringBuilder strBuilder = new SqlConnectionStringBuilder();
+            //strBuilder.DataSource = "localhost";
+            //connectionString =
+            //    //@"Data Source=localhost;User Name=root;Password=;Database=mydatabase;";
+            //    @"data source=127.0.0.1;Trusted_Connection=True;Database=mydatabase;";
+
+            //mComm = new SqlCommand(str, mConn);
+
+            //try
+            //{
+            //    mConn.Open();
+            //    mComm.ExecuteNonQuery();
+            //    MessageBox.Show("DataBase is Created Successfully"
+            //        , "MyProgram"
+            //        , MessageBoxButtons.OK
+            //        , MessageBoxIcon.Information);
+
+            //}catch(Exception ex)
+            //{
+            //    //MessageBox.Show(ex.ToString()
+            //    //    , "MyProgram"
+            //    //    , MessageBoxButtons.OK
+            //    //    , MessageBoxIcon.Information);
+            //}
+            //finally
+            //{
+            //    if (mConn.State == ConnectionState.Open)
+            //    {
+            //        //mConn.Close();
+            //    }
+            //}
+
+            //openDatabase();
+
+        }
+
+        public void queryTable(String cmd)
+        {
+            this.Validate();
+            mConn = new MySqlConnection(connectionString);
+            
+            mConn.Open();
+            //createTable();
+
+            mComm = new MySqlCommand(cmd, mConn);
+
+            mAdapter = new MySqlDataAdapter();
+            mAdapter.SelectCommand = mComm;
+            
+            mTable = new DataTable();
+
+            mTable.Load(mComm.ExecuteReader());
+
+            MySqlCommandBuilder builder = new MySqlCommandBuilder(mAdapter);
+
+            mAdapter.Update(mTable);
+            builder.GetUpdateCommand();
+            //dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = mTable;
+
+            mConn.Close();
+            
+        }
+
+        public void createTable()
+        {
+            try
+            {
+                MySqlCommand command = new MySqlCommand(tableCommand, mConn);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                //mTable = mTable.GetChanges();
+                //mAdapter.Update(mTable);
+                //dataGridView1.Update();
+                //cb.RefreshSchema();
+                //mAdapter.UpdateCommand = cb.GetUpdateCommand();
+                //mAdapter.Update(mTable);
+                //dataGridView1.DataSource = mTable;
+
+                mConn = new MySqlConnection(connectionString);
+                mConn.Open();
+                
+                mAdapter = new MySqlDataAdapter(tableSelection, mConn);
+                MySqlCommandBuilder builder = new MySqlCommandBuilder(mAdapter);
+
+                mAdapter.Update(mTable);
+                builder.GetUpdateCommand();
+                mConn.Close();
+                dataGridView1.Update();
+                this.Validate();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            mAdapter.Update(mTable);
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mConn.Open();
+            //delete();
+            delete2();
+            mConn.Close();
+        }
+
+        public void delete2()
+        {
+            try
+            {
+
+                Int32 selectedCellCount = this.dataGridView1
+                    .GetCellCount(DataGridViewElementStates.Selected);
+
+                foreach (DataGridViewRow row in this.dataGridView1.SelectedRows)
+                {
+                    dataGridView1.Rows.RemoveAt(row.Index);
+
+                    mConn = new MySqlConnection(connectionString);
+
+                    String data = row.Cells[0].Value.ToString();
+
+                    using (row)
+                    {
+                        queryTable("delete from qlnv2 where 'name' = "
+                            + row.Cells["NAME"]);
+                    }
+
+                    //mAdapter = new MySqlDataAdapter(tableSelection, mConn);
+                    //MySqlCommandBuilder builder = new MySqlCommandBuilder(mAdapter);
+                    //builder.GetDeleteCommand();
+                    //builder.GetUpdateCommand();
+                    
+                    dataGridView1.Update();
+                    }
+                
+
+                    //mAdapter.DeleteCommand = mComm;
+
+                    mConn.Close();
+                    this.Validate();
+                
+
+            }
+            catch (Exception ex) { };
+            this.mAdapter.Update(this.mTable);
+        }
+
+        public void delete()
+        {
+            try
+            {
+
+                Int32 selectedCellCount = this.dataGridView1
+                    .GetCellCount(DataGridViewElementStates.Selected);
+
+                foreach (DataGridViewRow row in this.dataGridView1.SelectedRows)
+                {
+                    dataGridView1.Rows.RemoveAt(row.Index);
+
+                    mConn = new MySqlConnection(connectionString);
+
+                    String data = row.Cells[0].Value.ToString();
+
+                    //mComm = new MySqlCommand("DELETE FROM 'qlnv2' WHERE ID = "
+                    //+ row.Cells[0] + ";",
+                    //mConn);
+
+                    using (mConn)
+                    {
+                        //    MySqlCommandBuilder cmd = new MySqlCommandBuilder(mAdapter);
+
+                        //    mComm = new MySqlCommand("DELETE FROM 'qlnv2' WHERE ID = "
+                        //    + row.Cells[0] + ";",
+                        //    mConn);
+
+                        //    mComm = cmd.GetDeleteCommand();
+                        //}
+                        mAdapter.DeleteCommand = new MySqlCommand(
+                        "DELETE FROM 'qlnv2' WHERE ID = "
+                        + row.Cells[0] + ";",
+                        mConn);
+                        //mConn.Open();
+
+                        MySqlCommandBuilder builder = new MySqlCommandBuilder(
+                            mAdapter);
+                        mAdapter.Update(mTable);
+                        //mConn.Close();
+                        dataGridView1.Update();
+                    }
+                    //mComm.ExecuteNonQuery();
+
+                    mAdapter.DeleteCommand = mComm;
+
+                    mConn.Close();
+                    this.Validate();
+
+                }
+
+                this.mAdapter.Update(this.mTable);
+
+            }
+            catch (Exception ex) { };
+        }
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            //connectionString = @"data source=" + tbIP.Text
+            //    + ";Trusted_Connection=True;Database=mydatabase;"
+            //    + "username=root;"
+            //    + "password=;";
+
+            //connect to xampp database
+            connectionString = "datasource=127.0.0.1;username=root;"
+                + "password=;database=mydatabase;";
+
+            //connectionString = "Data Source=DESKTOP-DM3AGPH\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=True";
+
+            //connectionString = "datasource=185.28.21.151;username=u920308848_bang;"
+            //    + "password=nhoem115;database=u920308848_data;";
+
+            //connectionString = "server=mysql.hostinger.vn;"
+            //    +"port=1234;"
+            //    + "database=u920308848_data;"
+            //    + "uid=u920308848_bang;"
+            //    + "password=********;";
+
+            //connectionString = "datasource=basebear.com/login.aspx?ext=1&g=dc1cc10a-4ff1-4276-bdf8-5160dbc7bba1;"
+            //    + "trustedconnection=true;"
+            //    + "database=qlnv2";
+
+            //connect database 4free
+            //connectionString = "datasource=db4free.net;username=phatnvm;"
+            //    + "password=magickiss;database=phatnvm_database;"
+            //    + "Connection Timeout=60";
+
+            queryTable(tableSelection);
+
+            foreach(DataRow row in mTable.Rows)
+            {
+                tbIP.Text = row["NAME"].ToString();
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
